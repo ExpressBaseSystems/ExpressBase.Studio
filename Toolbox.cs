49663b625e721f6d00f88080c58d269eea808b1d
sureba73 <sureba@expressbase.com>
2017-01-31 15:47:00 +0530

    ToolBox new entries for combo etc

--- Toolbox.cs
+++ Toolbox.cs
@@ -17,28 +17,24 @@ namespace ExpressBase.Studio
             listBox1.Items.Add(toolPointer);
             //- the controls
             listBox1.Items.Add(new ToolboxItem(typeof(EbButtonControl)));
-            listBox1.Items.Add(new ToolboxItem(typeof(ListView)));
-            listBox1.Items.Add(new ToolboxItem(typeof(TreeView)));
             listBox1.Items.Add(new ToolboxItem(typeof(EbTextBoxControl)));
-            listBox1.Items.Add(new ToolboxItem(typeof(EbPasswordControl)));
-            listBox1.Items.Add(new ToolboxItem(typeof(NumericUpDown)));
-            listBox1.Items.Add(new ToolboxItem(typeof(MaskedTextBox)));
-            listBox1.Items.Add(new ToolboxItem(typeof(RadioButton)));
-            listBox1.Items.Add(new ToolboxItem(typeof(DateTimePicker)));
-            listBox1.Items.Add(new ToolboxItem(typeof(PictureBox)));
-            listBox1.Items.Add(new ToolboxItem(typeof(RichTextBox)));
-            listBox1.Items.Add(new ToolboxItem(typeof(Label)));
-            listBox1.Items.Add(new ToolboxItem(typeof(LinkLabel)));
-            listBox1.Items.Add(new ToolboxItem(typeof(OpenFileDialog)));
-            listBox1.Items.Add(new ToolboxItem(typeof(CheckBox)));
-            listBox1.Items.Add(new ToolboxItem(typeof(ComboBox)));
-            listBox1.Items.Add(new ToolboxItem(typeof(GroupBox)));
-            listBox1.Items.Add(new ToolboxItem(typeof(ImageList)));
-            //listBox1.Items.Add(new ToolboxItem(typeof(EbTabControl)));
+            listBox1.Items.Add(new ToolboxItem(typeof(EbNumericControl)));
+            listBox1.Items.Add(new ToolboxItem(typeof(EbDateControl)));
+            listBox1.Items.Add(new ToolboxItem(typeof(EbComboBoxControl)));
             listBox1.Items.Add(new ToolboxItem(typeof(EbChartControl)));
             listBox1.Items.Add(new ToolboxItem(typeof(EbDataGridViewControl)));
             listBox1.Items.Add(new ToolboxItem(typeof(EbTableLayoutPanel)));
             listBox1.Items.Add(new ToolboxItem(typeof(EbSplitContainer)));
+
+            //listBox1.Items.Add(new ToolboxItem(typeof(EbTabControl)));
+            //listBox1.Items.Add(new ToolboxItem(typeof(OpenFileDialog)));
+            //listBox1.Items.Add(new ToolboxItem(typeof(CheckBox)));
+            //listBox1.Items.Add(new ToolboxItem(typeof(RadioButton)));
+            //listBox1.Items.Add(new ToolboxItem(typeof(GroupBox)));
+            //listBox1.Items.Add(new ToolboxItem(typeof(PictureBox)));
+            //listBox1.Items.Add(new ToolboxItem(typeof(RichTextBox)));
+            //listBox1.Items.Add(new ToolboxItem(typeof(Label)));
+            //listBox1.Items.Add(new ToolboxItem(typeof(LinkLabel)));
         }
     }
 }
